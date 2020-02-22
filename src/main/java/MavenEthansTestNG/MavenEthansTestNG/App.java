0bf345a2d11b6e0535bc0b6fb3cc8ad22d8f8ada
Ashutosh2906 <ashutosh.kharatmol@gmail.com>
2020-02-22 10:58:21 +0530

    this is my first project

---
+++ src/main/java/MavenEthansTestNG/MavenEthansTestNG/App.java
@@ -0,0 +1,13 @@
+package MavenEthansTestNG.MavenEthansTestNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MavenEthansTestNG/MavenEthansTestNG/NewTest.java
@@ -0,0 +1,12 @@
+package MavenEthansTestNG.MavenEthansTestNG;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void hello() {
+	  
+	  Hello obj=new Hello();
+	  System.out.println("Hello");
+  }
+}
